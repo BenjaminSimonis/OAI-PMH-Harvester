55c537b6a2d4026c6c039e8dd285380e02848361
Benjamin Simonis <10655564+glomran@users.noreply.github.com>
2019-02-12 04:39:53 -0500

    Modified downloader

--- downloader.py
+++ downloader.py
@@ -14,14 +14,15 @@ def download(link, rec):
     r = urlopen(req)
     filename = str(r.headers['content-disposition']).split("filename=")[1].replace('"', '')
     oaid = link.split("/")[-1]
-    if not os.path.exists(downloadpath + oaid):
-        os.makedirs(downloadpath + oaid)
-        target = downloadpath + oaid + "/" + filename
-        wget.download(link, target)
-        if fnmatch.filter(os.listdir(downloadpath + oaid), "*.zip"):
-            with zipfile.ZipFile(target, 'r') as zip_ref:
-                zip_ref.extractall(downloadpath + oaid)
-                filelist = os.listdir(downloadpath + oaid)
-                for file in filelist:
-                    if ("zip" or "json") not in file:
-                        validator.main(downloadpath + oaid + "/" + file, downloadpath + oaid + "/")
+    path = downloadpath + oaid
+    if not os.path.exists(path):
+        os.makedirs(path)
+    target = path + "/" + filename
+    wget.download(link, target)
+    if fnmatch.filter(os.listdir(path), "*.zip"):
+        with zipfile.ZipFile(target, 'r') as zip_ref:
+            zip_ref.extractall(path)
+    filelist = os.listdir(path)
+    for file in filelist:
+        if ("zip" or "json") not in file:
+            validator.main(path + "/" + file, path + "/")
