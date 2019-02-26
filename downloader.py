6e9a6b042cd99211d291e98437c83038e4ac1a1c
Benjamin Simonis <10655564+glomran@users.noreply.github.com>
2019-02-26 11:11:40 +0100

    Unzipping all zips in a download.zip

--- downloader.py
+++ downloader.py
@@ -8,6 +8,7 @@ from constants import get_constants as c
 from mdparser import parser
 import validator
 
+downloadzip = False
 
 def download(link, rec):
 
@@ -21,11 +22,31 @@ def download(link, rec):
     target = path + c().SLASH() + filename
     wget.download(link, target)
     if fnmatch.filter(os.listdir(path), "*.zip"):
-        with zipfile.ZipFile(target, 'r') as zip_ref:
-            zip_ref.extractall(path)
-    filelist = os.listdir(path)
+        filelist = unpack_zip(path)
+    else:
+        filelist = os.listdir(path)
     parser(filename, rec, path)
     for file in filelist:
         if ("zip" or "json") not in file:
             validator.main(path + c().SLASH() + file, path + c().SLASH())
+    for file in filelist:
+        if file is not "download.zip":
+            os.remove(path + c().SLASH() + file)
     return True
+
+
+def unpack_zip(path):
+    f_list = os.listdir(path)
+    for file in f_list:
+        if ".zip" in file:
+            with zipfile.ZipFile(path, 'r') as zip_ref:
+                if downloadzip is False and file is "download.zip":
+                    zip_ref.extractall(path)
+                    downloadzip = True
+                elif file is not "download.zip":
+                    zip_ref.extractall(path)
+                if file is not "download.zip":
+                    os.remove(path + c().SLASH() + file)
+            unpack_zip(path)
+        else:
+            return f_list
